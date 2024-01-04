f0036f6d22967fe32b49b6fc300f42909c5931e3
DOUGLAS <114367885+Douglas4735@users.noreply.github.com>
2024-01-04 19:15:58 -0400

    Update aula01-for.js
    alteração na aspas

--- estrutura-for/aula01-for.js
+++ estrutura-for/aula01-for.js
@@ -3,10 +3,10 @@ const nome = 'douglas s. s';
 // for (let i = 0; i < nome.length; i++) {
 //     console.log(nome[i]);
     
-// }
+// };
 
 for (let i = 0; i < nome.length; i++) {
     const letra = nome[i];
     console.log(letra);
     
-}
+};
