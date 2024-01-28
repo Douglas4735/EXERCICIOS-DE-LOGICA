7cab0edb059d6d6f522843e77f443d0daf203987
douglas4735 <douglas4735@gmail.com>
2024-01-28 16:29:55 -0400

    commits

--- EXERCICIOS/salário/mediaAluno.js
+++ EXERCICIOS/salário/mediaAluno.js
@@ -1,10 +1,26 @@
 
 const {gets, print} = require('./main');
 
-const valorSalarioBruto = gets();
+const valorSalario = gets();
 const valorAdicionalDosBeneficios = gets();
 
 function calcularPorcentagem(valor,percentual){
     return valor * (percentual / 100);
 }
-print(calcularPorcentagem(valorSalarioBruto, 10));
+print(calcularPorcentagem(valorSalario, 10));
+
+function pegarAliquota(salario){
+    
+    if(salario >= 0 && salario <= 2500){
+        return 10;
+    }else{
+        return 15;
+    }
+}
+const aliquotaImposto =  pegarAliquota(valorSalario);
+
+const valorImposto = calcularPorcentagem(valorSalario, aliquotaImposto);
+
+const valorAtransferir = valorSalario - valorImposto + valorAdicionalDosBeneficios;
+
+ print(valorAtransferir);
